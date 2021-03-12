a7a7a132595384762a646b8bd9e02ac986e3b535
vishalsonar <vsonar908@gmail.com>
2021-03-12 02:47:11 -0800

    global exception handled

---
+++ src/main/java/com/sonar/vishal/exception/HindException.java
@@ -0,0 +1,16 @@
+package com.sonar.vishal.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import com.sonar.vishal.logic.HindLogic;
+
+public class HindException implements ExceptionMapper<Throwable> {
+
+	@Override
+	public Response toResponse(Throwable exception) {
+		String response = new HindLogic().getFailedResponse();
+		return Response.ok().entity(response).build();
+	}
+
+}
